0c3d624122246a77997024fb887b5005605d0f0e
Nathan Coleman <nathandanielcoleman@gmail.com>
2020-01-26 18:50:31 -0500

    Define initial Terraform for DynamoDB table

---
+++ dynamo.tf
@@ -0,0 +1,10 @@
+resource "aws_dynamodb_table" "priority-queue-table" {
+  name = "PriorityQueue"
+  hash_key = "id"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
