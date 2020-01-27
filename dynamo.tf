8a9d1f91008e01c112a46101d2443fc30ca984b8
Nathan Coleman <nathandanielcoleman@gmail.com>
2020-01-26 19:06:52 -0500

    Add Global Secondary Index to DynamoDB table

--- dynamo.tf
+++ dynamo.tf
@@ -11,4 +11,21 @@ resource "aws_dynamodb_table" "priority-queue-table" {
     name = "id"
     type = "S"
   }
+
+  attribute {
+    name = "priority"
+    type = "N"
+  }
+
+  attribute {
+    name = "enqueued_time"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "priority-enqueued_time-index"
+    hash_key = "priority"
+    range_key = "enqueued_time"
+    projection_type = "ALL"
+  }
 }
